fa8d6668e19303d1d71cca3d93347164e0e49066
vish-47 <vish.sriramoju@gmail.com>
2021-10-06 19:28:07 +0530

    changed

--- src/test/java/new_test.java
+++ src/test/java/new_test.java
@@ -10,5 +10,14 @@ public class new_test {
         WebDriverManager.chromedriver().setup();
         driver =new ChromeDriver();
         driver.get("http://www.google.com");
+        System.out.println(driver.getCurrentUrl());
+        driver.manage().window().setSize(new Dimension(1440, 875));
+        driver.findElement(By.name("q")).sendKeys("amazon");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        driver.findElement(By.cssSelector(".eKjLze .LC20lb")).click();
+        System.out.println(driver.getCurrentUrl());
+
+
+
     }
 }
